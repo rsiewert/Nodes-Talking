69a58baeb385096ee460afcce2590e2c76e64880
Ron Siewert <ron.siewert@alchemysoft.com>
2014-02-09 15:10:29 -0500

    have message passing and display working

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+/*
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+io.sockets.on('connection', function (socket) {
+	console.log("socket connected....");
+	socket.emit('news', { hello: 'world' });
+	
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+
+	socket.on('my test event', function (data) {
+		console.log(data);
+	});
+
+});
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
 var express = require('express'),
 	http = require('http'),
 	path = require('path'),
@@ -12,13 +47,19 @@ app.configure(function() {
 	//app.use(express.static(path.join(__dirname,'public')));
 	app.set('views',__dirname + '/views');
 	app.set('view engine','jade');
+	app.set('view options', { pretty: true });
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 });
 
-http.createServer(app).listen(app.get('port'),function() {
+var server = http.createServer(app).listen(app.get('port'),function() {
 	console.log("RabbitMQ + Node.js app running on " + app.get('port') + "!");
 });
+
+//setup socket.io to listen to our app
+var io = require('socket.io').listen(server);
+
+
 
 console.log("The views path is: " + app.get('views'));
 
@@ -29,7 +70,7 @@ app.queueStatus = 'No queue established';
 app.get('/',function(req,res) {
 	console.log("inside get /");
 	
-	res.render(app.get('views') + '/index.jade',
+	res.render('index',
 		{
 			title:'Welcome to RabbitMQ and Node/Express',
 			connectionStatus:app.connectionStatus,
@@ -42,11 +83,15 @@ app.get('/',function(req,res) {
 
 app.post('/start-server',function(req,res) {
 	console.log("Inside start-server");
+	
+
+	
 	app.rabbitMqConnection = amqp.createConnection({host:'localhost'});
 	app.rabbitMqConnection.on('ready',function() {
-		app.connectionStatus = 'Connected';
+		app.connectionStatus = 'RabbitMQ is Connected';
 		res.redirect('/');
 	});
+	
 });
 
 app.post('/new-exchange',function(req,res) {
@@ -62,6 +107,7 @@ app.post('/new-queue',function(req,res) {
 	app.q = app.rabbitMqConnection.queue('test-queue',{},function(queue) {
 		console.log('Queue ' + queue.name + ' is open');
 		app.queueStatus = 'The queue is ready for use!';
+		app.q.bind(app.e,'#');
 	});
 	
 	res.redirect('/');
@@ -69,7 +115,6 @@ app.post('/new-queue',function(req,res) {
 
 app.get('/message-service',function(req,res) {
 	console.log('inside get message-service');
-	app.q.bind(app.e,'#');
 	//app.v = jade.compile(app.get('views') + '/message-server.jade',{debug:true,pretty:true});
 	//console.log('html = ' + app.v({title:'Welcome',sentMessage:'Hello'}));
 	/*
@@ -81,19 +126,19 @@ app.get('/message-service',function(req,res) {
 	)
 	res.render(htmlout);
 
+
 	jade.renderFile(app.get('views') + '/message-service.jade',
 			{debug:true,pretty:true,title:'Welcome to the messaging service',sentMessage:'Hello World'},
 			function(err,html) {
 				if(err) throw err;
 				console.log('html = ' + html);
-				res.render(html);
+				//res.render(html);
 			});
-			
-	*/
-	res.render(app.get('views') + '/message-service.jade',
+	*/			
+	res.render('message-service',
 	   {
 			title:'Welcome to the messaging service',
-	     	sentMessage: 'Hello World'
+	     	sentMessage: ''
 	   });
 	 
 });
@@ -110,8 +155,35 @@ app.post('/newMessage',function(req,res) {
 	app.q.subscribe(function(msg,headers,deliveryInfo) {
 		console.log('inside subscribe: msg.message = ' + msg.message);
 		console.log('got a message with routing key = ' + deliveryInfo.routingKey);
-	
-/*	
+
+/*
+		
+		jade.renderFile(app.get('views') + '/message-service.jade',
+			{debug:true,pretty:true,title:'Welcome to the messaging service',sentMessage:msg.message},
+			function(err,html) {
+				if(err) throw err;
+				console.log('html = ' + html);
+				//res.send(html);
+				console.log("after res.send");
+			});
+
+			
+		res.render('message-service',
+			{
+				title: "You have new mail!",
+				sentMessage: msg.message
+			});
+*/
+		io.sockets.on('connection',function(socket) {
+			console.log("Sockets.io is Connected!!");
+			socket.emit('message',{"sentMessage":msg.message})
+			socket.on("my return event",function(data) {
+				console.log(data);
+			});
+			
+		});
+
+		/*
 	var htmlout = app.v(
 		{
 			title: "Welcome to my new messaging service",
@@ -119,14 +191,18 @@ app.post('/newMessage',function(req,res) {
 		}
 	)
 	res.render(htmlout);
-*/
+	
 		
-		res.render(app.get('views') + '/message-service.jade',
+		res.render('message-service',
 			{
 				title: "You have new mail!",
 				sentMessage: msg.message
 			});
+	*/	
+	
 	});
 	
-	console.log('after subscribe');
+	res.redirect('/message-service');
+	console.log('after sockets.io');
 });
+
