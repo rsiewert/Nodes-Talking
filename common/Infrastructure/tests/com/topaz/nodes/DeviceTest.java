d0f8263c6e069bfb136dab8d376382ddf7a8de66
rbgodwin <randy.godwin@gmail.com>
2014-04-30 13:12:31 +0400

    checkpoint changes

---
+++ common/Infrastructure/tests/com/topaz/nodes/DeviceTest.java
@@ -0,0 +1,13 @@
+package com.topaz.nodes;
+
+import org.junit.Test;
+
+/**
+ * Created by rbgodwin on 4/26/14.
+ */
+public class DeviceTest {
+    @Test
+    public void testBuilder() throws Exception {
+
+    }
+}
---
+++ common/Infrastructure/tests/com/topaz/nodes/NodeTest.java
@@ -0,0 +1,36 @@
+package com.topaz.nodes;
+
+/**
+ * Created by rbgodwin on 4/26/14.
+ */
+public class NodeTest {
+    @org.junit.Test
+    public void testBuilder() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetActsAs() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetNodeId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetLocation() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetStatus() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddRestProtocol() throws Exception {
+
+    }
+}
