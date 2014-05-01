6cf55e92845196aa7f99c5e51839c9ceecb63e14
rbgodwin <randy.godwin@gmail.com>
2014-05-01 17:36:45 +0400

    Tweaks for Junit test of Rest endpoint /json-mirror

--- common/Infrastructure/tests/com/topaz/communications/servers/RestServerTest.java
+++ common/Infrastructure/tests/com/topaz/communications/servers/RestServerTest.java
@@ -4,19 +4,17 @@ import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.Assert;
 
-
 /**
  * Created by rbgodwin on 4/30/14.
  */
 public class RestServerTest {
 
     final static String REST_SERVER = "localhost:3000";
-    final static String TEST_JSON = "{\"best\":\"message\"}";
     final static String[] TEST_JSON_STRINGS = {"test1", "test2", "test3"};
 
 
     @Test
-    public void testGetJson() throws Exception {
+    public void testJsonMirror() throws Exception {
 
         String response;
         RestServer testServer = new RestServer(this.REST_SERVER);
